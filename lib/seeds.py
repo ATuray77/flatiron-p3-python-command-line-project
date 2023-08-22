9935e9611e5eb4b80466d3146b03ad6b3cdc05a9
Aruna Turay <tura_arun@yahoo.com>
2023-08-22 01:21:37 -0400

    creates engine and sessionmaker

--- lib/seeds.py
+++ lib/seeds.py
@@ -3,3 +3,7 @@ from models import Artist, Song
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+engine = create_engine('sqlite:///data.db')
+
+Session = sessionmaker(bind=engine)  # enables communication with the db
+session = Session()
