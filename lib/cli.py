172e02f8460e02889604be2579ce3e4921e6d512
Aruna Turay <tura_arun@yahoo.com>
2023-08-23 15:23:46 -0400

    adds simple_term_menu and pretty cli packages

---
+++ lib/cli.py
@@ -0,0 +1,7 @@
+from prettycli import red
+from simple_term_menu import TerminalMenu
+from models import Owner
+
+class Cli():
+    def __init__(self):
+        pass
--- lib/seed.py
+++ lib/seed.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+
 from models import Artist, Song
 
 from sqlalchemy import create_engine
